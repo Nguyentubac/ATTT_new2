6206198b38eed1f6204e339d395eff56c54a1725
Nguyentubac <102511709+Nguyentubac@users.noreply.github.com>
2024-11-04 22:14:31 +0700

    Initial commit

---
+++ FormPlayFair.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ATTT
+{
+    partial class FormPlayFair
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Key = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textkq = new System.Windows.Forms.Label();
+            this.dataGridView12 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView12)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(52, 277);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 28);
+            this.label1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(103, 94);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(357, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Key
+            // 
+            this.Key.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Key.Location = new System.Drawing.Point(103, 158);
+            this.Key.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Key.Name = "Key";
+            this.Key.Size = new System.Drawing.Size(357, 30);
+            this.Key.TabIndex = 3;
+            this.Key.TextChanged += new System.EventHandler(this.Key_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Mã Hoá",
+            "Giải Mã"});
+            this.comboBox1.Location = new System.Drawing.Point(759, 101);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 33);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(576, 97);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Phương pháp ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 162);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Key";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(47, 101);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "P";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(47, 226);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Kết Quả";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textkq
+            // 
+            this.textkq.AutoSize = true;
+            this.textkq.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textkq.ForeColor = System.Drawing.Color.Red;
+            this.textkq.Location = new System.Drawing.Point(167, 226);
+            this.textkq.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.textkq.Name = "textkq";
+            this.textkq.Size = new System.Drawing.Size(0, 25);
+            this.textkq.TabIndex = 12;
+            // 
+            // dataGridView12
+            // 
+            this.dataGridView12.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView12.Location = new System.Drawing.Point(501, 188);
+            this.dataGridView12.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView12.Name = "dataGridView12";
+            this.dataGridView12.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView12.RowTemplate.Height = 24;
+            this.dataGridView12.Size = new System.Drawing.Size(615, 459);
+            this.dataGridView12.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1196, 735);
+            this.Controls.Add(this.dataGridView12);
+            this.Controls.Add(this.textkq);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Key);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView12)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Key;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label textkq;
+        private System.Windows.Forms.DataGridView dataGridView12;
+    }
+}
+
---
+++ FormPlayFair.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ATTT
+{   
+    public partial class FormPlayFair : Form
+    {
+        DataTable table = new DataTable();
+        class PlayFair
+        {
+
+            
+            public char[,] CreateTable(string key)
+            {
+                
+
+                key = key.ToUpper().Replace("J", "I"); // thay J bằng I
+                HashSet<char> usedChars = new HashSet<char>();
+                List<char> chars = new List<char>();
+
+                foreach (char c in key)
+                {
+                    if (char.IsLetter(c) && !usedChars.Contains(c))
+                    {
+                        usedChars.Add(c);
+                        chars.Add(c);
+                    }
+                }
+
+                for (char c = 'A'; c <= 'Z'; c++)
+                {
+                    if (c != 'J' && !usedChars.Contains(c))
+                    {
+                        usedChars.Add(c); // kí tự đang xét đã sử dụng chưa
+                        chars.Add(c);
+                    }
+                }
+                // tạo bảng playfair
+                char[,] table = new char[5, 5];
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        table[i, j] = chars[i * 5 + j];
+                        
+                    }
+                }
+                return table;
+            }
+            static int[] FindCharacter(char[,] table, char target)
+            {
+                for (int i = 0; i < table.GetLength(0); i++)
+                {
+                    for (int j = 0; j < table.GetLength(1); j++)
+                    {
+                        if (table[i, j] == target)
+                        {
+                            return new int[] { i, j };
+                        }
+                    }
+                }
+                return null;
+            }
+            public string Decrypt(string message, char[,] table)
+            {
+                message = message.ToUpper().Replace("J", "I").Replace(" ", "");
+                if (message.Length % 2 != 0)
+                {
+                    message += "X"; // Thêm ký tự nếu lẻ
+                }
+                string result = "";
+
+                for (int i = 0; i < message.Length; i += 2)
+                {
+                    char first = message[i];
+                    char second = message[i + 1];
+                    int[] pos1 = FindCharacter(table, first);
+                    int[] pos2 = FindCharacter(table, second);
+
+                    if (pos1[0] == pos2[0]) // Cùng hàng
+                    {
+                        result += table[pos1[0], (pos1[1] +4 ) % 5];
+                        result += table[pos2[0], (pos2[1] + 4) % 5];
+                    }
+                    else if (pos1[1] == pos2[1]) // Cùng cột  
+                    {
+                        result += table[(pos1[0] +4) % 5, pos1[1]];
+                        result += table[(pos2[0] +4) % 5, pos2[1]];
+                    }
+                    else // Hình chữ nhật
+                    {
+                        result += table[pos1[0], pos2[1]];
+                        result += table[pos2[0], pos1[1]];
+                    }
+                }
+                return result;
+            }
+
+            
+            public string Encrypt(string message, char[,] table)
+            {
+                
+                message = message.ToUpper().Replace("J", "I").Replace(" ", "");
+                if (message.Length % 2 != 0)
+                {
+                    message += "X"; // Thêm ký tự nếu lẻ
+                }
+
+                string result = "";
+
+                for (int i = 0; i < message.Length; i += 2)
+                {
+                    char first = message[i];
+                    char second = message[i + 1];
+                    int[] pos1 = FindCharacter(table, first);
+                    int[] pos2 = FindCharacter(table, second);
+
+                    if (pos1[0] == pos2[0]) // Cùng hàng
+                    {
+                        result += table[pos1[0], (pos1[1] + 1) % 5];
+                        result += table[pos2[0], (pos2[1] + 1) % 5];
+                    }
+                    else if (pos1[1] == pos2[1]) // Cùng cột
+                    {
+                        result += table[(pos1[0] + 1) % 5, pos1[1]];
+                        result += table[(pos2[0] + 1) % 5, pos2[1]];
+                    }
+                    else // Hình chữ nhật
+                    {
+                        result += table[pos1[0], pos2[1]];
+                        result += table[pos2[0], pos1[1]];
+                    }
+                }
+                return result; 
+            }
+        }
+        public FormPlayFair()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        public DataTable ConvertArrayToDataTable(char[,] array2D)
+        {
+            DataTable dt = new DataTable();
+
+            for (int col = 0; col < array2D.GetLength(1); col++)
+            {
+                dt.Columns.Add("Column " + (col + 1));
+            }
+
+            // Thêm các hàng vào DataTable
+            for (int row = 0; row < array2D.GetLength(0); row++)
+            {
+                DataRow dr = dt.NewRow();
+                for (int col = 0; col < array2D.GetLength(1); col++)
+                {
+                    dr[col] = array2D[row, col];
+                }
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+
+        public bool kiemtranumber(string num)
+        {
+            foreach (char c in num)
+            {
+                if (char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length==0 || Key.Text.Length==0)
+            {
+                MessageBox.Show("Các trường thông tin chưa nhập đầy đủ");
+            }
+            else
+            {
+                if(kiemtranumber(textBox1.Text.ToString()))
+                {
+                    if (comboBox1.SelectedIndex != -1)
+                    {
+                        PlayFair playfair = new PlayFair();
+                        char[,] table = playfair.CreateTable(Key.Text);
+                        string cb = comboBox1.SelectedItem.ToString();
+                        if (cb.Equals("Mã Hoá"))
+                        {
+                            string encryptedMessage = playfair.Encrypt(textBox1.Text, table);
+                            textkq.Text = encryptedMessage;
+                        }
+                        if (cb.Equals("Giải Mã"))
+                        {
+                            string descryptedMessage = playfair.Decrypt(textBox1.Text, table);
+                            textkq.Text = descryptedMessage;
+                        }
+
+                        DataTable dt = ConvertArrayToDataTable(table);
+                        dataGridView12.DataSource = dt;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Chưa chọn phương thức giải mã hoặc mã hoá");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Các trường thông tin chỉ nhận chữ cái");
+                }
+                
+            }    
+           
+            
+        }
+        
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Key_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadData()
+        {
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
